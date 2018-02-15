cb8ffef5d0c6caf4bc6894346e87e619594757c2
Daniel <danielmorillo249@hotmail.com>
2018-02-15 14:02:48 +0100

    added generic jagged array initializer

---
+++ source/Services/initmap.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace GameLibrary.Services {
+// initializes a rectangular jagged array
+// takes type and 2 dimensions
+static class MapInitializer {
+    public static T[][] CreateArray<T>(int  rows, int cols) {
+        T[][] rectArray = new T[rows][];
+
+        for(int i = 0; i < rectArray.GetLength(0); i++) {
+            rectArray[i] = new T[cols];
+        }
+        return rectArray;
+    }
+}
+}
