0911eb384c3916ae386c0f727f72b620e275f9f9
Daniel <danielmorillo249@hotmail.com>
2018-02-17 20:37:22 +0100

    printables dont have screen reference

--- source/Graphics/iprintable.cs
+++ source/Graphics/iprintable.cs
@@ -7,9 +7,6 @@ public interface IPrintable {
     // texture
     Texture Texture { get; }
 
-    // reference to Screen
-    Screen Screen { get; }
-
     // positiont
     int Position_x { get; }
     int Position_y { get; }
