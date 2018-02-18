38f0aa2d7107728056d06d90595d8b84890a712d
Daniel <danielmorillo249@hotmail.com>
2018-02-18 17:38:16 +0100

    added pixel class

---
+++ source/Graphics/pixel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GameLibrary.Graphics {
+class Pixel {
+
+    // character
+    public char Char { get; set; }
+    
+    // color
+    public ConsoleColor BackgroundColor { get; set; }
+    public ConsoleColor ForegroundColor { get; set; }
+
+}
+}
